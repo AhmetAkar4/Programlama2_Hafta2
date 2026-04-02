480a0e5f92c92af9478b4bbcd24537b5d4d2a26b
Ahmet Akar <84382368+AhmetAkar4@users.noreply.github.com>
2026-04-02 03:07:00 +0300

    Programlama2_Hafta2

---
+++ Hafta2_Odev/Hafta2_Odev/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru 2
+            string kelime = "game";
+            Stack<char> harfStack = new Stack<char>();
+
+            foreach (char harf in kelime)
+                harfStack.Push(harf);
+
+            Console.Write("Ters çevrilmiş: ");
+            while (harfStack.Count > 0)
+                Console.Write(harfStack.Pop());
+            Console.WriteLine("\n");
+
+            // Soru 5
+            string metin = "(Oyun(Geliştirme))";
+            Stack<char> parantezStack = new Stack<char>();
+            bool dengeli = true;
+
+            foreach (char c in metin)
+            {
+                if (c == '(')
+                    parantezStack.Push(c);
+                else if (c == ')')
+                {
+                    if (parantezStack.Count == 0)
+                    {
+                        dengeli = false;
+                        break;
+                    }
+                    parantezStack.Pop();
+                }
+            }
+
+            if (parantezStack.Count > 0) dengeli = false;
+            Console.WriteLine($"Parantezler dengeli mi?: {dengeli}\n");
+
+            // Soru 4 - PriorityQueue Olmayan Sürümler İçin Alternatif Çözüm
+            List<KeyValuePair<string, int>> aiGorevleri = new List<KeyValuePair<string, int>>();
+            aiGorevleri.Add(new KeyValuePair<string, int>("Run", 0));
+            aiGorevleri.Add(new KeyValuePair<string, int>("Attack", 1));
+            aiGorevleri.Add(new KeyValuePair<string, int>("Chase", 2));
+            aiGorevleri.Add(new KeyValuePair<string, int>("Patrol", 5));
+
+            // Öncelik değerine (Value) göre küçükten büyüğe sıralıyoruz
+            var siraliGorevler = aiGorevleri.OrderBy(g => g.Value).ToList();
+
+            Console.WriteLine("Görev Sırası:");
+            foreach (var gorev in siraliGorevler)
+            {
+                Console.WriteLine($"{gorev.Key} ({gorev.Value})");
+            }
+        }
+    }
+}
---
+++ Hafta2_Odev/Hafta2_Odev/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Hafta2_Odev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hafta2_Odev")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b5c8c265-dd68-46e3-a6d4-13bfd7304ce4")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
